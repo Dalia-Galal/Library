69b721ae28d905bf5c44664fe0a645c02131935b
Dalia Galal <dollagalal@gmail.com>
2025-02-05 17:50:48 +0200

    add book function

--- Library.cpp
+++ Library.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
+#include <string>
+#include <stdlib.h>
 using namespace std;
 
+void addBook(int* bookNumber, string* bookTitle, string* bookAuthor, int& size);
 int main()
 {   
     int size ;
@@ -12,4 +15,26 @@ int main()
     *bookTitle = { " " };
     string *bookAuthor = new string[size];
     *bookAuthor = { " " };
+}
+void addBook(int* bookNumber, string* bookTitle, string* bookAuthor,int &size)
+{  
+    for (int i = 0; i < size; i++)
+    { 
+        char tempBookNum;
+        cin >> tempBookNum;
+        if(isdigit(tempBookNum))
+        {
+            bookNumber[i] = tempBookNum - 48;
+        }
+        else
+        {   
+            cout << " enter a number \n";
+            return addBook(bookNumber,bookTitle, bookAuthor,size);
+        }
+           
+        cin.ignore();
+        
+        getline(cin,bookTitle[i],'\n');
+        getline(cin,bookAuthor[i],'\n');
+    }
 }
